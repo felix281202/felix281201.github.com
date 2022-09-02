f38590470a8c2086bcabb6285d232dfda5884e30
felix281202 <74926564+felix281202@users.noreply.github.com>
2022-09-02 17:46:01 -0400

    agrgarmos proyecto boletines

---
+++ boletines/javascript/mian.js
@@ -0,0 +1,39 @@
+/*console.log("Desde un archivo externo javascript hola");
+
+let container = document.querySelector(".container");*/
+/*
+let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+    td.addEventListener("click",function(){
+        console.log("click");
+    });
+});*/
+/*
+let cerrar = document.querySelectorAll(".close");
+
+cerrar.forEach(function(x){
+    x.addEventListener("click",function(ev){
+        ev.preventDefault();
+        return false;
+    });
+
+});*/
+
+let links = document.querySelectorAll(".close");
+links.forEach(function(link){
+    link.addEventListener("click", function(ev){
+        ev.preventDefault();
+        let content = document.querySelector('.content');
+        content.classList.remove("animate__animated");
+        content.classList.remove("animate__fadeInUp");
+        content.classList.add("animate__animated");
+        content.classList.add("animate__bounce");
+
+        setTimeout(function(){
+            location.href = "/";
+        }, 1000);
+    });
+
+    return false;
+});
